2b7b74cfc66fcf9179ec7a44e05f44487f0e7f25
pelivan <pelivan@kolesa.kz>
2025-05-08 04:13:47 +0500

    first commit

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,6 @@ import { fileURLToPath, URL } from 'node:url'
 
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -13,5 +12,8 @@ export default defineConfig({
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
     },
+  },
+  server: {
+    port: 3000,  // Устанавливаем порт 3000
   },
 })
