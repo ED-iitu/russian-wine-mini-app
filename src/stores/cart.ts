855a5b0c31350b7456a0a359a154f48b15fc8e17
pelivan <pelivan@kolesa.kz>
2025-05-08 03:38:56 +0500

    first commit

--- src/stores/cart.ts
+++ src/stores/cart.ts
@@ -2,7 +2,7 @@ import { defineStore } from 'pinia'
 
 export const useCartStore = defineStore('cart', {
     state: () => ({
-        items: [],
+        items: [] as Record<string, any>[],  // Используем Record, чтобы объекты могли иметь любые свойства
     }),
     actions: {
         loadCart() {
@@ -12,7 +12,7 @@ export const useCartStore = defineStore('cart', {
         saveCart() {
             localStorage.setItem('cart', JSON.stringify(this.items))
         },
-        add(wine) {
+        add(wine: Record<string, any>) {
             const existing = this.items.find(item => item.id === wine.id)
             if (existing) {
                 existing.quantity += 1
@@ -21,7 +21,7 @@ export const useCartStore = defineStore('cart', {
             }
             this.saveCart()
         },
-        remove(id) {
+        remove(id: any) {
             this.items = this.items.filter(item => item.id !== id)
             this.saveCart()
         },
